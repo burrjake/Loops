816eae62d32ed082805a883dfbe65d0412200500
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-27 08:51:05 -0600

    add for loop

--- Loops.vb
+++ Loops.vb
@@ -1,6 +1,15 @@
-﻿Module Loops
+﻿Option Explicit On
+Option Strict On
+Option Compare Binary
+
+
+Module Loops
 
     Sub Main()
+        Dim index As Integer
+        For index = 0 To 10 Step 1
+            Console.ReadLine()
+        Next
 
     End Sub
 
